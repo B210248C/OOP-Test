347510de168a95251076f58d87802f6a4ff524b1
B210248C <128680122+B210248C@users.noreply.github.com>
2024-07-29 16:59:22 +0800

    Delete Business.java

--- Business.java
+++
@@ -1,8 +0,0 @@
-class Business extends Seat {
-    protected Business(){
-
-    }
-    public Business(String seatNumber, boolean availability, Flight flight, double price) {
-        super(seatNumber, availability, flight, "Business", price);
-    }
-}
